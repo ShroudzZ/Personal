52d598c2b850a4101286e35156f1d327286336e6
Shroudzz <1833549764@qq.com>
2019-03-20 01:32:56 +0800

    v1.0

---
+++ MyProject/MyProject/Issue.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyProject
+{
+    public class Issue
+    {
+        public int Count { get; set; }
+        public List<string> Subject { get; set; }
+        public int Min { get; set; } = 1;
+        public int Max { get; set; } = 9;
+        private List<string> _type = new List<string> { "-", "+", "/", "*" };
+        public Issue(int count)
+        {
+            Count = count;
+            CreateSubject();
+        }
+
+        private void CreateSubject()
+        {
+            string result = "";
+            Subject = new List<string>();
+            for (var i = 0; i < Count; i++)
+            {
+                for (var j = 0; j < 4 ; j++)
+                {
+                    Thread.Sleep(20);
+                    int num = new Random().Next(Min, Max);
+                    int index = new Random().Next(0, _type.Count);
+                    result += _type[index] + num;
+                    _type.RemoveAt(index);
+                    
+                    if (j == 3)
+                    {
+                        result = result.Substring(1, result.Length-1);
+                        Subject.Add(result);
+                    }
+                }
+                _type = new List<string> { "-", "+", "/", "*" };
+                result = "";
+            }
+
+        }
+    }
+}
---
+++ MyProject/MyProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Issue i = new Issue(10);
+            DataTable dt = new DataTable();
+            for (var j = 0; j < i.Subject.Count; j++)
+            {
+
+                Console.WriteLine(j + 1 + "、 " + i.Subject[j] + " = {0}", dt.Compute(i.Subject[j], ""));
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
